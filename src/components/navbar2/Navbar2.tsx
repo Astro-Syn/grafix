08fcb922e42d900fae9af3e24972bb6bf199f38e
Astro-Syn <kelseyamyb@gmail.com>
2026-04-15 13:11:16 -0400

    styling

--- src/components/navbar2/Navbar2.tsx
+++ src/components/navbar2/Navbar2.tsx
@@ -7,10 +7,10 @@ export default function Navbar2() {
     return (
         <div className='navbar2-wrapper'>
             Navbar 2 (menu)
-            <div>
+            <div className='nav-button-wrapper'>
             <Link to={'/dashboard'}>
-            <LuClipboard />
-            My Boards
+            <LuClipboard color={'white'} size={50}/>
+            <p> My Boards</p>
             </Link>
             </div>
         </div>
